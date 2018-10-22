809528f18f14680ecdf21b90860c4fc62b57c9c5
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-10-22 11:40:30 -0400

    Added a validation that will check it the User (googleId) already exists and them will create the user if NOT.

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,9 +13,19 @@ passport.use(
             callbackURL:'/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            new User({
-                googleId:profile.id
-            }).save();
+            User.findOne({ googleId:profile.id })
+                .then( existingUser => {
+                    if (existingUser) {
+                        console.log("Existing:", profile.id)
+                        done(null, existingUser);
+                    }else{
+                        new User({ googleId:profile.id }).save()
+                        .then( user => {
+                            console.log("Created:", profile.id)
+                            done(null, user)
+                        });
+                    }
+                });
         }
     )
 );
