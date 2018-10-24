9567ae445c04049e3177ba5c07a56697e7c23007
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-10-24 19:46:37 -0400

    Refactor the passport Strategy using async/await

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,20 +23,16 @@ passport.use(
             callbackURL:'/auth/google/callback',
             proxy:true
         }, 
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId:profile.id })
-                .then( existingUser => {
-                    if (existingUser) {
-                        console.log("Existing:", profile.id)
-                        done(null, existingUser);
-                    }else{
-                        new User({ googleId:profile.id }).save()
-                        .then( user => {
-                            console.log("Created:", profile.id)
-                            done(null, user)
-                        });
-                    }
-                });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser =  await User.findOne({ googleId:profile.id })
+            if (existingUser) {
+                console.log("Existing:", profile.id)
+                done(null, existingUser);
+            }else{
+                const user = await new User({ googleId:profile.id }).save()
+                console.log("Created:", profile.id)
+                done(null, user)
+            }
         }
     )
 );
