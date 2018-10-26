6feecca455ed5e17ce44340732d6bdb9a467b321
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-10-26 15:44:42 -0400

    Removed a debbuger from the StripePayments component

--- server/client/src/components/StripePayments.js
+++ server/client/src/components/StripePayments.js
@@ -3,13 +3,18 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class StripePayments extends Component {
     render(){
-        debugger;
         return(
             <StripeCheckout 
+                name="Emaily"
+                description="Get 5 email credits for US$ 5"
                 amount={500} // cents
                 token={token => console.log("StripeToken:",token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">
+                    Add credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
