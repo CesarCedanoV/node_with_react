a4a1e728ca957eba2e8a9a46c17a97b29401b558
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-11-02 19:38:22 -0400

    Cleaning the formFields when move outside the SurveyNew component and also added the redirect to the Dashboard when submit the Survey using withRouter and History from 'react-router-dom'

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({onCancel, formValues}) => {
+
+const SurveyFormReview = ({onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({name, label}) => {
     return (
       <div key={name}>
@@ -23,7 +26,9 @@ const SurveyFormReview = ({onCancel, formValues}) => {
       >
       Back </button>
       <button
-        className="green white-text btn-flat right">
+        className="green white-text btn-flat right"
+        onClick={() => submitSurvey(formValues, history)}
+      >
         Send Survey 
         <i className="material-icons right">email</i>
       </button>
@@ -37,4 +42,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
