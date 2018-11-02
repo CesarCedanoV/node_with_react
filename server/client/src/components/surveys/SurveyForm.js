f49fb57809eb5e1af263e925cff0064b2f932655
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-11-02 00:03:48 -0400

    Added the SurveyField and use it for the FIELDS of the Survey in the SurveyForm and also added an Error for Empty

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,18 +1,40 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
+
+const FIELDS = [
+    {name:'title', label:'Title'},
+    {name:'subject', label:'Subject'},
+    {name:'body', label:'Email body'},
+    {name:'recipients', label:'Send to'}
+]
 
 class SurveyForm extends Component {
+    renderFields(){
+        return _.map(FIELDS, ({name,label}) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+        });
+    }
+
     render() {
         return (
             <div>
                 SurveyForm!
                 <form onSubmit={this.props.handleSubmit(values => console.log(values) )}>
-                    <Field 
-                        type="text"
-                        name="surveyTitle"
-                        component="input"
-                    />
-                    <button type="submit">Submit</button>
+                    {this.renderFields()}
+                    <Link to="/surveys">
+                        <button type="cancel" className="red btn-flat left white-text">
+                            Cancel
+                            <i className="material-icons right">arrow_back</i>
+                        </button>
+                    </Link>
+                   
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
                 
             </div>
@@ -20,6 +42,18 @@ class SurveyForm extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    _.each(FIELDS, ({name}) => {
+        if (!values[name]){
+            errors[name] = `You must provide a value.`;
+        }
+    });
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form:'surveyForm'
 })(SurveyForm);
