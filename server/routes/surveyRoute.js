bbbbee43b99e0f042e6330b638da739dbfe07776
CesarCedanoV <CesarCedanoV@gmail.com>
2018-11-05 11:55:52 -0400

    Installed lodash and path-parser to iterate the webhook events coming to our API

--- server/routes/surveyRoute.js
+++ server/routes/surveyRoute.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,38 +10,53 @@ const surveyTemplate = require('../services/emailTemplate/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/:surveyId/:surveyResponse', (req,res) => {
-        res.send('Thanks for voting');
+  app.get('/api/surveys/:surveyId/:surveyResponse', (req,res) => {
+    res.send('Thanks for voting');
+  });
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email:email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
     });
-    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-        const { title, subject, body, recipients } = req.body;
 
-        const survey = new Survey({
-            title,
-            subject,
-            body,
-            recipients: recipients.split(',').map(email => ({ email:email.trim() })),
-            _user: req.user.id,
-            dateSent: Date.now()
-        });
-
-        const mailer = new Mailer(survey,surveyTemplate(survey));
-        
-        try{
-            await mailer.send();
-            await survey.save();
-            req.user.credits -= 1;
-            const user = await req.user.save();
+    const mailer = new Mailer(survey,surveyTemplate(survey));
     
-            res.send(user);
-        }catch(err){
-            res.status(422).send(err);
-        }
-    });
+    try{
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
 
-    app.post('/api/surveys/webhooks', (req,res) => {
-        console.log(req.body);
-        res.send({});
-    });
+      res.send(user);
+    }catch(err){
+      res.status(422).send(err);
+    }
+  });
+
+  app.post('/api/surveys/webhooks', (req,res) => {
+    const pathParser = new Path('/api/surveys/:surveyId/:surveyResponse');
+
+    const events = _.chain(req.body)
+      .map(({email, url}) => {
+        const match = pathParser.test(new URL(url).pathname);
+        if (match) {
+          return {email, surveyId: match.surveyId, surveyResponse: match.surveyResponse}
+        }
+      })
+      .compact()
+      .uniqBy('email','surveyId')
+    .value();
+
+    
+    console.log(events);
+
+    res.send({});
+  });
 
 };
